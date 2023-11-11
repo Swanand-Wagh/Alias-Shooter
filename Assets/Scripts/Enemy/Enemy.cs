03c78bcd8a7092bacc6e56428a2a943e0ce6d68f
Swanand-Wagh <swanandwagh7@gmail.com>
2023-11-10 18:20:00 -0800

    enemy follows the player using NavMeshAgent

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 [RequireComponent(typeof(EnemyController))]
+[RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : MonoBehaviour
 {
+    private NavMeshAgent pathfinder;
+    private Transform player;
+
     void Start()
     {
-
+        pathfinder = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void Update()
     {
-
+        pathfinder.SetDestination(player.position);
     }
 }
